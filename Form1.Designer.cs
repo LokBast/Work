c919e6366130bc2ac3dddfa39a7a15d35808e28d
Константин Кошелев <cool.xatiko2014@mail.ru>
2019-09-02 06:05:21 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(398, 257);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.ListView1_ItemSelectionChanged_1);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(416, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(118, 257);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сортировка";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(540, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(226, 257);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Редактирование";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 306);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            RadioButton MyRadioButton;
+            TextBox txt;
+            Label MyLabel;
+
+            int Xr = 12, Yr = 25, Xb = 110, Yb = 21, Xl = 12, Yl = 25;
+
+            foreach (PropertyInfo prop in typeof(DataObject).GetProperties())
+            {
+                //создание столбцов                
+                listView1.Columns.Add("Field" + i, 100);
+
+
+                //создание радио кнопки
+                MyRadioButton = new RadioButton();
+                MyRadioButton.Click += RadioButtonSelect;
+                MyRadioButton.Tag = i;
+                MyRadioButton.Text = "Field" + i;
+                MyRadioButton.Location = new Point(Xr, Yr);
+                MyRadioButton.AutoSize = true;
+                groupBox1.Controls.Add(MyRadioButton);
+                Yr += 25;
+
+
+                //СОЗДАНИЕ ТЕКСТОВОГО ПОЛЯ
+                txt = new TextBox();
+                txt.Leave += LeaveTxt;
+                txt.Enter += EnterTxt;
+                txt.Tag = i;
+                txt.Name = "textBox" + i;
+                txt.Text = "";
+                txt.Location = new Point(Xb, Yb);
+                txt.AutoSize = true;
+                groupBox2.Controls.Add(txt);
+                Yb += 25;
+
+
+                //СОЗДАНИЕ ЛЕЙБЛА
+                MyLabel = new Label();
+                MyLabel.Name = "Label" + i;
+                MyLabel.Text = "";
+                MyLabel.Location = new Point(Xl, Yl);
+                MyLabel.AutoSize = true;
+                groupBox2.Controls.Add(MyLabel);
+                Yl += 25;
+
+
+                i++;
+            }
+
+            Type t = typeof(DataObject); int j = 0;
+            foreach (var prop in t.GetProperties())
+            {
+                groupBox2.Controls.Find("Label" + j, false)[0].Text = "" + prop.Name + " " + prop.PropertyType.Name;
+                j++;
+            }
+        }
+
+        public class DataObject
+        {
+            private string _field0;
+            private DateTime _field1;
+            private int _field2;
+            //private long _field3;
+
+            public string Field0
+            {
+                get { return _field0; }
+                set { _field0 = value; }
+            }
+
+            public DateTime Field1
+            {
+                get { return _field1; }
+                set { _field1 = value; }
+            }
+
+            public int Field2
+            {
+                get { return _field2; }
+                set { _field2 = value; }
+            }
+
+            //public long Field3
+            //{
+            //    get { return _field3; }
+            //    set { _field3 = value; }
+            //}
+        }
+
+
+
+
+        //ДОБАВЛЕНИЕ ДАННЫХ В LISTVIEW
+        DataObject dataObject = new DataObject();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Type t = typeof(DataObject); int j = 0;
+            foreach (var prop in t.GetProperties())
+            {
+                if (prop.PropertyType.Name == groupBox2.Controls.Find("TextBox2", false)[0].Text) { }
+                j++;
+            }
+
+            ListViewItem item1 = new ListViewItem("" + (groupBox2.Controls.Find("TextBox0", false))[0].Text);
+            for (int m = 1; m < i; m++) { item1.SubItems.Add("" + (groupBox2.Controls.Find("TextBox" + m, false))[0].Text); }
+
+            listView1.Items.AddRange(new ListViewItem[] { item1 });
+            sort();
+        }
+
+
+
+
+
+        //ЗАМЕНА ДАННЫХ ЧЕРЕЗ ТЕКСТБОКС
+        string txtEnt; int ent;
+        public void EnterTxt(object sender, EventArgs e)
+        {
+            PropertyInfo[] props = typeof(DataObject).GetProperties();
+            for (int v = 0; v < i; v++)
+            {
+                if (groupBox2.Controls.Find("TextBox" + v, false)[0].Focused == true)
+                {
+                    txtEnt = groupBox2.Controls.Find("TextBox" + v, false)[0].Text;
+                    ent = v;
+                }
+            }
+        }
+        public void LeaveTxt(object sender, EventArgs e)
+        {
+            //ПРОВЕРКА ВВОДИМЫХ ДАННЫХ
+            PropertyInfo[] props = typeof(DataObject).GetProperties();
+            try
+            {
+                var t = Convert.ChangeType(groupBox2.Controls.Find("TextBox" + ent, false)[0].Text, props[ent].PropertyType);
+            }
+            catch
+            {
+                groupBox2.Controls.Find("TextBox" + ent, false)[0].Focus();
+
+                MessageBox.Show(
+                "Невозможно привести к типу " + props[ent].PropertyType,
+                "Ошибка введенных данных",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+                return;
+            }
+
+
+            //ЗАМЕНА ДАННЫХ В LISTVIEW
+            foreach (ListViewItem eachItem in listView1.SelectedItems)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+                for (int j = 0; j < i; j++)
+                {
+                    item.SubItems[j].Text = groupBox2.Controls.Find("TextBox" + j, false)[0].Text;
+                }
+            }
+            sort();
+        }
+
+
+
+
+        //ПЕРЕНОС ДАННЫХ ТАБЛИЦЫ В ТЕКСТБОКС
+        int ind;
+        public void ListView1_ItemSelectionChanged_1(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            ind = e.ItemIndex;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (((RadioButton)groupBox1.Controls[j]).Checked == true) { g = j; }
+            }
+            this.listView1.ListViewItemSorter = new ListViewItemComparer(g, listView1.Sorting);
+        }
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(ind);
+            foreach (ListViewItem eachItem in listView1.SelectedItems)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+
+                for (int j = 0; j < i; j++) { groupBox2.Controls.Find("TextBox" + j, false)[0].Text = (item.SubItems[j].Text); }
+            }
+        }
+
+
+
+        //УДАЛЕНИЕ СТРОКИ
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem eachItem in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(eachItem);
+            }
+            sort();
+        }
+
+
+
+        //СОРТИРОВКА
+        int g = 0;
+        public void sort()
+        {
+            ListView lv1 = new ListView();
+            for (int j = 0; j < i; j++)
+            {
+                if (((RadioButton)groupBox1.Controls[j]).Checked == true) { g = j; }
+            }
+            this.listView1.ListViewItemSorter = new ListViewItemComparer(g, listView1.Sorting);
+        }
+
+        public void RadioButtonSelect(object sender, EventArgs e)
+        {
+            sort();
+        }
+
+        public class ListViewItemComparer : IComparer
+        {
+
+            private int col;
+            private SortOrder order;
+            public ListViewItemComparer(int column, SortOrder order)
+            {
+                col = column;
+                this.order = order;
+            }
+            public int Compare(object x, object y)
+            {
+                int returnVal = -1;
+                returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text, ((ListViewItem)y).SubItems[col].Text);
+                return returnVal;
+            }
+        }
+    }
+}
